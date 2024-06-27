bcb632a1453e8bbd7bc69ca13306af6c7f72ec7b
신희성 <vvwv3092@gmail.com>
2024-06-27 21:05:43 +0900

    init

---
+++ src/main/java/com/example/redisresponsecache/RedisResponseCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisresponsecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisResponseCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisResponseCacheApplication.class, args);
+    }
+
+}
